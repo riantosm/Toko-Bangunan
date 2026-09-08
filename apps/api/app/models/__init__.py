89a69301e7c93eb5b3756bf123bc686d6221ac86
Rian Tosm <rian.tosm@gmail.com>
2026-09-08 20:52:36 +0700

    feat(api): step_types + workflow_steps models and migration

--- apps/api/app/models/__init__.py
+++ apps/api/app/models/__init__.py
@@ -3,5 +3,15 @@ from app.models.order import Order, OrderItem
 from app.models.processed_request import ProcessedRequest
 from app.models.product import Product
 from app.models.user import User
+from app.models.workflow import StepType, WorkflowStep
 
-__all__ = ["Job", "Order", "OrderItem", "ProcessedRequest", "Product", "User"]
+__all__ = [
+    "Job",
+    "Order",
+    "OrderItem",
+    "ProcessedRequest",
+    "Product",
+    "StepType",
+    "User",
+    "WorkflowStep",
+]
